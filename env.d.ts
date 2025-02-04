55086bd2705b8fcdd4ed7711a55e5bedfaded300
mcowin <vinvladik@gmail.com>
2025-02-04 20:49:04 +0300

    added URI to env

--- env.d.ts
+++ env.d.ts
@@ -1,6 +1,7 @@
 declare namespace NodeJS {
-  interface ProcessEnv {
-    SPOTIFY_CLIENT_ID: string;
-    SPOTIFY_CLIENT_SECRET: string;
-  }
+    interface ProcessEnv {
+        SPOTIFY_CLIENT_ID: string;
+        SPOTIFY_CLIENT_SECRET: string;
+        URI: string;
+    }
 }
